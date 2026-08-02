ec30318f65d429992d86f4339586b4d9fb450ba7
alexanderjosehild-tech <alexander.jose.hild@gmail.com>
2026-08-02 17:18:53 +0200

    Add files via upload

---
+++ src/sanity/image.ts
@@ -0,0 +1,10 @@
+import createImageUrlBuilder from "@sanity/image-url";
+import type { Image } from "sanity";
+import { projectId, dataset } from "./client";
+
+const builder = createImageUrlBuilder({ projectId, dataset });
+
+export function urlFor(source: Image | undefined | null) {
+  if (!source) return undefined;
+  return builder.image(source);
+}
